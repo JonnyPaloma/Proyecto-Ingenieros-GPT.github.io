bcb1a066520222dd4fc8653738738e2118d5b9df
JonnyPaloma <jonnyleandropaloma@gmail.com>
2026-09-09 21:35:49 -0500

    conexion.js

--- conexion.js
+++ conexion.js
@@ -1,7 +1,7 @@
 // conexion.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://elige-tu-vida.vercel.app/';
+const SUPABASE_URL = 'https://rwhpkuobljjolyjxgvgh.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_pyxeGlvqoy3LLneDqQRxKw_hJbU077N';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
